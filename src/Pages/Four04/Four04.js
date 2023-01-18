360c547664e9d006e1955b403427ce227d28f325
Osama Khan <47086954+osamality@users.noreply.github.com>
2023-01-18 11:44:25 +0400

    first commit
    first commit

--- src/Pages/Four04/Four04.js
+++ src/Pages/Four04/Four04.js
@@ -8,7 +8,7 @@ class Four04 extends Component {
           <div className="container">
             <div className="row h-100 align-items-center justify-content-center text-center">
               <div className="col-12">
-                <h1>The page you’re looking for can’t be found. try again</h1>
+                <h1>The page you’re looking for can’t be found.</h1>
               </div>
             </div>
           </div>
